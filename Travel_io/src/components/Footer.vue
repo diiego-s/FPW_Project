d6f015b2bc13f45f3717a35612ce1fcb9a259f10
diiego-s <serradiego2005@icloud.com>
2025-08-03 18:54:30 +0200

    aggiunto bordi stondati

--- Travel_io/src/components/Footer.vue
+++ Travel_io/src/components/Footer.vue
@@ -4,14 +4,3 @@
             <p><i>da<br>mettere un <br>indirizzo</i></p>
         </footer>
 </template>
-
-<style>
-    footer{
-        background: linear-gradient(90deg, var(--primary) 10%, var(--tertiary));
-        color: var(--white);
-        text-align: left;
-        padding: 20px;
-        font-size: 14px;
-       
-    }
-</style>
