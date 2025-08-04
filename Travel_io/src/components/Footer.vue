ef04851424bb51315f6cd55b002e5ed9008d7a98
diiego-s <serradiego2005@icloud.com>
2025-08-04 14:48:34 +0200

    aggiunto layout a tre colonne

--- Travel_io/src/components/Footer.vue
+++ Travel_io/src/components/Footer.vue
@@ -1,6 +1,11 @@
 <template>
      <footer>
             <p>travel_io</p>
-            <p><i>da<br>mettere un <br>indirizzo</i></p>
+            <ul>
+                <li>Diego Serra - 60/61/66505</li>
+                <li>Giulia Cannata - 60/61/66464</li>
+                <li>Michele Chillotti - 60/61/66441</li>
+                <li>Matteo Manai - 60/61/ 66438</li>
+            </ul>
         </footer>
 </template>
