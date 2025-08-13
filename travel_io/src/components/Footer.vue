e19b82d497633d911f5fe1132efec030a249a8d2
diiego-s <serradiego2005@icloud.com>
2025-08-13 12:09:05 +0200

    miglioramento footer

--- travel_io/src/components/Footer.vue
+++ travel_io/src/components/Footer.vue
@@ -1,11 +1,27 @@
 <template>
      <footer>
-            <p>travel_io</p>
-            <ul>
-                <li>Diego Serra - 60/61/66505</li>
-                <li>Giulia Cannata - 60/61/66464</li>
-                <li>Michele Chillotti - 60/61/66441</li>
-                <li>Matteo Manai - 60/61/ 66438</li>
+            <h3 id="footerTitle">travel_io</h3>
+            <ul id="footerContent">
+                <li>Diego Serra - <a href="mailto:d.serra126@studenti.unica.it">d.serra126@studenti.unica.it</a> - 60/61/66505</li>
+                <li>Giulia Cannata - <a href="mailto:g.cannata4@studenti.unica.it">g.cannata4@studenti.unica.it</a> - 60/61/66464</li>
+                <li>Michele Chillotti - <a href="mailto:m.chillotti22@studenti.unica.it">m.chillotti22@studenti.unica.it</a> - 60/61/66441</li>
+                <li>Matteo Manai - <a href="mailto:m.manai22@studenti.unica.it">m.manai22@studenti.unica.it</a> - 60/61/ 66438</li>
             </ul>
         </footer>
 </template>
+
+<style>
+    #footerTitle{
+        color: var(--white);
+    }
+
+    #footerContent a{
+        color: var(--white);
+        font-size: 15px;
+        text-decoration: underline;
+    }
+
+    #footerContent a:hover{
+        font-weight: normal;
+    }
+</style>
