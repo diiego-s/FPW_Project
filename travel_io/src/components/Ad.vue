37daf001f934066bae15f0d83ff0592817f3774c
diiego-s <serradiego2005@icloud.com>
2025-09-22 18:24:28 +0200

    aggiunto le pubblicità, da rivedere le dimensioni

--- travel_io/src/components/Ad.vue
+++ travel_io/src/components/Ad.vue
@@ -1,5 +1,42 @@
+<script>
+ export default{
+    methods:{
+        randInt(){
+            let i = Math.floor(Math.random() * 100)+1;
+            return i%2===0;
+        }
+    }
+ }
+</script>
+
 <template>
     <div class="col-3">
-         <p>pubblicit&agrave;</p>
+        <div id="boxAd">
+            <img v-if="randInt()" src="../assets/img/ad/ad_en.png" alt="english advertisement">
+            <img v-else src="../assets/img/ad/ad_it.png" alt="publicita italiana">
+        </div>
       </div>
 </template>
+
+<style>
+    #boxAd img{
+        border-radius: 25px;
+        margin-top: 5px;
+        margin-bottom: 10px;
+        text-align: center;
+    }
+
+    @media only screen and (min-width: 768px){
+        #boxAd img{
+            height: 873px;
+            width:310px;
+        }
+    }
+
+    @media only screen and (max-width: 768px){
+        #boxAd img{
+            height: 600px;
+            width: 100%;
+        }
+    }
+</style>
--- travel_io/src/components/InfoBar.vue
+++ travel_io/src/components/InfoBar.vue
@@ -3,26 +3,28 @@
 </script>
 
 <template>
-    <div id="infobar" class="col-1">
-         <article>
-        <h2>Cosa facciamo</h2>
-        <p>Con travel_io puoi trovare e prenotare pacchetti vacanza ed esperienze su misura, scegliendo tra centinaia di destinazioni. Grazie a una piattaforma leggera e reattiva, ti aiutiamo a confrontare le opzioni migliori in pochi secondi, ovunque ti trovi.</p>
-
-        <h3>Perché scegliere travel_io</h3>
-        <ul>
-            <li>⚡ Interfaccia semplice e intuitiva</li>
-            <li>🌍 Ampia scelta di destinazioni e attività</li>
-            <li>💡 Filtri intelligenti per personalizzare ogni ricerca</li>
-            <li>🔒 Prenotazioni sicure e gestione trasparente</li>
-            <li>📱 Accessibile da desktop, tablet e smartphone</li>
-        </ul>
-
-        <h3>I nostri valori</h3>
-        <p>In travel_io crediamo che ogni viaggio debba essere un’esperienza positiva, sia per chi parte sia per chi accoglie. Per questo ci impegniamo a promuovere un turismo consapevole, sostenibile e rispettoso delle culture locali. Collaboriamo solo con partner affidabili, selezionati per qualità e serietà.</p>
-
-        <p>Hai bisogno di aiuto? Visita la nostra pagina contatti o scrivici: il nostro team è sempre pronto a supportarti, dalla prenotazione alla partenza.</p>   
-    </article>
-      </div>
+    <div class="col-1">
+        <div id="infobar">
+            <article>
+            <h2>Cosa facciamo</h2>
+            <p>Con travel_io puoi trovare e prenotare pacchetti vacanza ed esperienze su misura, scegliendo tra centinaia di destinazioni. Grazie a una piattaforma leggera e reattiva, ti aiutiamo a confrontare le opzioni migliori in pochi secondi, ovunque ti trovi.</p>
+    
+            <h3>Perché scegliere travel_io</h3>
+            <ul>
+                <li>⚡ Interfaccia semplice e intuitiva</li>
+                <li>🌍 Ampia scelta di destinazioni e attività</li>
+                <li>💡 Filtri intelligenti per personalizzare ogni ricerca</li>
+                <li>🔒 Prenotazioni sicure e gestione trasparente</li>
+                <li>📱 Accessibile da desktop, tablet e smartphone</li>
+            </ul>
+    
+            <h3>I nostri valori</h3>
+            <p>In travel_io crediamo che ogni viaggio debba essere un’esperienza positiva, sia per chi parte sia per chi accoglie. Per questo ci impegniamo a promuovere un turismo consapevole, sostenibile e rispettoso delle culture locali. Collaboriamo solo con partner affidabili, selezionati per qualità e serietà.</p>
+    
+            <p>Hai bisogno di aiuto? Visita la nostra pagina contatti o scrivici: il nostro team è sempre pronto a supportarti, dalla prenotazione alla partenza.</p>   
+        </article>
+          </div>
+        </div>
 </template>
 
 <style>
@@ -32,8 +34,8 @@
         border-radius: 25px;
         margin-bottom: 5px;
         margin-top: 5px;
-        padding-left: 70px;
-        padding-right: 70px;
+        padding-left: 25px;
+        padding-right: 25px;
         padding-top: 10px;
         padding-bottom: 10px;
     }
